e8a4cfd922eeddf98e3873b0f0e7dd59e167e538
George Lake <george.lake9@gmail.com>
2021-03-28 21:01:23 +1100

    quick start: stuck on window not found error

---
+++ QuickStartRen/App.js
@@ -0,0 +1,23 @@
+import { Bitcoin, Ethereum } from "@renproject/chains";
+import RenJS from "@renproject/ren";
+import ethers from "ethers";
+
+const mint = async () => {
+    await window.ethereum.enable();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    provider.getSigner();
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+
+    const lockAndMint = await new RenJS("testnet").lockAndMint({
+        asset: "BTC",
+        from: Bitcoin(),
+        to: Ethereum(provider.provider).Address(address),
+    });
+
+    console.log(`Deposit BTC to ${lockAndMint.gatewayAddress}`);
+
+    lockAndMint.on("deposit", RenJS.defaultDepositHandler);
+};
+
+mint().catch(console.error);
