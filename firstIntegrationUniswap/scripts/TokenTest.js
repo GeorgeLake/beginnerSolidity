a2d96b988fa2161bd6bdfe1322c2a8415709307a
George Lake <george.lake9@gmail.com>
2021-04-14 11:40:42 +1000

    integration of ERC20 tokens

--- firstIntegrationUniswap/scripts/TokenTest.js
+++ firstIntegrationUniswap/scripts/TokenTest.js
@@ -3,8 +3,10 @@ const AToken = artifacts.require('AToken')
 const BToken = artifacts.require('BToken')
 
 module.exports = async function (callback) {
+    // create instances of ERC20 tokens
     aToken = await AToken.deployed()
     bToken = await BToken.deployed()
+    // print out some info
     console.log(bToken.totalSupply())
     callback()
 }
